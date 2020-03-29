2b7f072dfcc0d691b407ad008a153aa4a8ce2509
Anubhab Sen <anubhabsen@gmail.com>
2020-03-28 23:00:17 -0700

    readying kicking env

--- pybulletgym/envs/roboschool/envs/locomotion/walker2d_soccer_env.py
+++ pybulletgym/envs/roboschool/envs/locomotion/walker2d_soccer_env.py
@@ -1,4 +1,4 @@
-from pybulletgym.envs.roboschool.envs.locomotion.walker_base_env import WalkerBaseBulletEnv
+from pybulletgym.envs.roboschool.envs.locomotion.walker_soccer_env import WalkerBaseBulletEnv
 from pybulletgym.envs.roboschool.robots.locomotors.walker_soccer import WalkerSoccer
 # from pybulletgym.envs.roboschool.robots.locomotors import Walker2D
 
