5f7798eab99da4f2091b9a991304534aa94a6574
mprocks <saurabhravindranath@gmail.com>
2020-03-30 18:28:52 -0700

    More Soccer Changes

--- pybulletgym/envs/roboschool/envs/locomotion/walker2d_soccer_env.py
+++ pybulletgym/envs/roboschool/envs/locomotion/walker2d_soccer_env.py
@@ -1,4 +1,4 @@
-from pybulletgym.envs.roboschool.envs.locomotion.walker_soccer_env import WalkerBaseBulletEnv
+from pybulletgym.envs.roboschool.envs.locomotion.walker_base_env import WalkerBaseBulletEnv
 from pybulletgym.envs.roboschool.robots.locomotors.walker_soccer import WalkerSoccer
 import numpy as np
 # from pybulletgym.envs.roboschool.robots.locomotors import Walker2D
@@ -9,6 +9,14 @@ class Walker2DSoccerBulletEnv(WalkerBaseBulletEnv):
         self.robot = WalkerSoccer()
         # self.robot = Walker2D()
         WalkerBaseBulletEnv.__init__(self, self.robot)
+        self.ball_previous_pos_x = 1
+        self.ball_previous_pos_y = 0
+        # self.potential
+        # self.old_robot_pos_x = 0
+        # self.old_robot_pos_y = 0
+
+    # def get_rewards(self, self.ball_bonus, self.alive, self.progress, electricity_cost):
+    #     return self.ball_bonus, self.alive, self.progress, electricity_cost
 
     def step(self, a):
         if not self.scene.multiplayer:  # if multiplayer, action first applied to all robots, then global step() called, then _step() for all robots with the same actions
@@ -17,16 +25,21 @@ class Walker2DSoccerBulletEnv(WalkerBaseBulletEnv):
 
         state = self.robot.calc_state()  # also calculates self.joints_at_limit
 
-        alive = float(self.robot.alive_bonus(state[0] + self.robot.initial_z, self.robot.body_rpy[1]))   # state[0] is body height above ground, body_rpy[1] is pitch
-        done = alive < 0
+        self.alive = float(self.robot.alive_bonus(state[0] + self.robot.initial_z, self.robot.body_rpy[1]))   # state[0] is body height above ground, body_rpy[1] is pitch
+        done = self.alive < 0
         if not np.isfinite(state).all():
             print("~INF~", state)
             done = True
 
+        self.ball_bonus = 1000*np.linalg.norm([self.robot.flag.current_position()[1] - self.ball_previous_pos_y, self.robot.flag.current_position()[0] - self.ball_previous_pos_x])
+        self.ball_previous_pos_x = self.robot.flag.current_position()[0]
+        self.ball_previous_pos_y = self.robot.flag.current_position()[1]
+
         potential_old = self.potential
+        # self.curr_robot
         self.potential = self.robot.calc_potential()
-        progress = float(self.potential - potential_old)
-        # progress = 0
+        self.progress = 1*float(self.potential - potential_old)
+        # self.progress = 0
 
         feet_collision_cost = 0.0
         for i, f in enumerate(self.robot.feet):  # TODO: Maybe calculating feet contacts could be done within the robot code
@@ -45,10 +58,12 @@ class Walker2DSoccerBulletEnv(WalkerBaseBulletEnv):
         joints_at_limit_cost = float(self.joints_at_limit_cost * self.robot.joints_at_limit)
         debugmode = 0
         if debugmode:
-            print("alive=")
-            print(alive)
-            print("progress")
-            print(progress)
+            print("self.ball_bonus=")
+            print(self.ball_bonus)
+            print("self.alive=")
+            print(self.alive)
+            print("self.progress")
+            print(self.progress)
             print("electricity_cost")
             print(electricity_cost)
             print("joints_at_limit_cost")
@@ -57,12 +72,14 @@ class Walker2DSoccerBulletEnv(WalkerBaseBulletEnv):
             print(feet_collision_cost)
 
         self.rewards = [
-            alive,
-            progress,
+            self.ball_bonus,
+            self.alive,
+            self.progress,
             electricity_cost,
             joints_at_limit_cost,
             feet_collision_cost
         ]
+
         if debugmode:
             print("rewards=")
             print(self.rewards)
